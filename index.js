4fe30504f961eb0d9fd393655c3b791b855ea946
queckezz <fabian.eichenberger@gmail.com>
2014-05-04 19:18:01 +0200

    use harmony

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 
+'use strict'
+
 /**
  * Module dependencies.
  */
 
-var Queue = require('co-queue')
-var assert = require('assert')
-var slice = [].slice
+let Queue = require('co-queue')
+let assert = require('assert')
+let slice = [].slice
 
 /**
  * Event queue.
  */
 
-var queue = new Queue;
+let queue = new Queue;
 
 /**
  * Wraps `e.emit` to emit sse events.
@@ -22,11 +24,11 @@ var queue = new Queue;
  */
 
 module.exports = function (e) {
-  var emit = e.emit
+  let emit = e.emit
 
   e.emit = function (type) {
-    var args = slice.call(arguments, 1)
-    var out = ''
+    let args = slice.call(arguments, 1)
+    let out = ''
 
     // event with no arguments
     if (args.length == 0) {
@@ -34,17 +36,16 @@ module.exports = function (e) {
     }
 
     // event with arguments
-    args.forEach(function (arg, i) {
+    for (let arg in args) {
       if (typeof arg == 'object') arg = JSON.stringify(arg);
-
       out += send('event', type)
       out += send('data', arg)
-    })
+    }
 
-    // end
+    // end of a data set
     out += '\n'
 
-    // push to queue to maintain event order
+    // push event into `queue` to maintain event order
     queue.push({
       type: type,
       out: out
